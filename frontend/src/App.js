7135fdc0bbc7de3c4010b940712ba2ffac5c5f9d
TuranAsik <53354450+TuranAsik@users.noreply.github.com>
2019-07-29 15:14:07 +0200

    ..

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Items from './components/items/items';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Items/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
